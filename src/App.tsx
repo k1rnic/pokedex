f87eb554a25d6a38edbef48b46430e72924d5197
k1rnic <knikolskiy.io@gmail.com>
2021-05-13 02:16:57 +0300

    chore(git-hooks): add husky, lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,4 @@ import cn from 'classnames';
 import React from 'react';
 import classes from './App.module.scss';
 
-export default () => <div className={cn(classes.header)}>This is App root</div>;
+export default () => <div className={cn(classes.headers)}>This is App root</div>;
